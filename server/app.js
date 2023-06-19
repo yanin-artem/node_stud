f383c311d683b97474e88fa581bca8626aa1aea6
yanin-artem <yanin_artem@list.ru>
2023-06-19 15:39:46 +0300

    дз

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:8080",
+  },
+});
+
+app.get("/", async (req, res) => {
+  return res.send(123);
+});
+
+app.listen(3000, async () => {
+  console.log("server started");
+});
+
+io.on("connection", (socket) => {
+  console.log("подключен килент", socket);
+});
+
+httpServer.listen(3001);
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+/* eslint-disable */
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+const { io } = require("socket.io-client");
+// eslint-disable-next-line
+const socket = io("http://localhost:3001");
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
